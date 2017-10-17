43f6ee999db1a8757fca7a12d90cef4b5a2f3736
Diego Tsuyoshi <rend@localhost.localdomain>
2017-10-17 17:41:54 -0300

    Adicionado inserções no banco para testes

--- database.sql
+++ database.sql
@@ -58,4 +58,11 @@ alter table `comments` add constraint `comment_user_foreign` foreign key (`user_
 alter table `posts` add constraint `post_user_foreign` foreign key (`user_id`) references `users`(`id`); 
 
 # Some data _-_-_-_-_
-insert into `users` (`name`, `email`, `password`, `active`)
+insert into `users` (`name`, `email`, `password`) values
+ ('Diego Tsuyoshi', 'dttsuyoshi@gmail.com', '123456'),
+ ('Geovana Helena', 'geovanahsps@gmail.com', '123456');
+
+ insert into `posts` (`title`, `body`, `user_id`) values
+ ('Lorem Ipsum', 'In sem justo, commodo ut, suscipit at, pharetra vitae, orci. Duis sapien nunc, commodo et, interdum suscipit, sollicitudin et, dolor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam id dolor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos. Mauris dictum', 1),
+ ('Segundo Lorem Ipsum','Etiam posuere quam ac quam. Maecenas aliquet accumsan leo. Nullam dapibus fermentum ipsum. Etiam quis quam. Integer lacinia. Nulla est. Nulla turpis magna, cursus sit amet, suscipit a, interdum id, felis. Integer vulputate sem a nibh rutrum consequat. Maecenas lorem. Pellentesque pretium lectus id turpis. Etiam sapien elit, consequat eget, tristique non, venenatis quis, ante. Fusce wisi. Phasellus faucibus molestie nisl. Fusce eget urna. Curabitur vitae diam non enim vestibulum interdum. Nulla quis diam. Ut tempus purus at lorem. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nullam feugiat, turpis at pulvinar vulputate, erat libero tristique tellus, nec bibendum odio risus sit amet ante. Aliquam erat volutpat. Nunc auctor. Mauris pretium quam et urna. Fusce nibh. Duis risus. Curabitur sagittis hendrerit ante. Aliquam erat volutpat. Vestibulum erat nulla, ullamcorper nec, rutrum non, nonummy ac, erat. Duis condimentum augue id magna semper rutrum. Nullam justo enim, consectetuer nec, ullamcorper ac, vestibulum in, elit. Proin pede metus, vulputate nec, fermentum fringilla, vehicula vitae, justo. Fusce consectetuer risus a nunc. Aliquam ornare wisi eu metus. Integer pellentesque quam vel velit. Duis pulvinar.
+',2);
