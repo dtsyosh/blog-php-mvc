77e32f51452378cb254b73a39f711be6bb9b9e0d
Diego Tsuyoshi <rend@localhost.localdomain>
2017-10-17 20:38:44 -0300

    added bootstrap by composer

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
 
 require 'config.php';
+require __DIR__ . '/vendor/autoload.php';
 
-function __autoload($class) {
-    require LIBS . $class .".php";
+function libsAutoloader($class_name) {
+    include(LIBS . '/' . $class_name . '.php');
 }
 
+spl_autoload_register('libsAutoloader');
 
 $bootstrap = new Bootstrap();
 
