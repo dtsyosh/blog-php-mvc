b40fbe29e14d9593a734b1c45c0e23b29113b8d3
Diego Tsuyoshi <dttsuyoshi@gmail.com>
2017-10-18 08:11:03 -0300

    Add loadModel no libs/controller

--- libs/Controller.php
+++ libs/Controller.php
@@ -5,4 +5,17 @@ class Controller {
     function __construct() {
         $this->view = new View();
     }
+
+    public function _loadModel($name, $modelPath = 'models/') {
+
+        $path = $modelPath . $name . '_model.php';
+
+        if (file_exists($path)) {
+
+            require $modelPath . $name . '_model.php';
+
+            $classModel = $name . '_Model';
+            $this->model = new $classModel();
+        }
+    }
 }
