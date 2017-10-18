10664b40661aabc5cd2aa6d2468c787828fb13cb
Diego Tsuyoshi <dttsuyoshi@gmail.com>
2017-10-18 13:06:46 -0300

    refactorando autoloader

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 require 'config.php';
 require __DIR__ . '/vendor/autoload.php';
 
-function libsAutoloader($class_name) {
+function autoloader($class_name) {
     include(LIBS . '/' . $class_name . '.php');
 }
 
-spl_autoload_register('libsAutoloader');
+spl_autoload_register('autoloader');
 
 $bootstrap = new Bootstrap();
 
