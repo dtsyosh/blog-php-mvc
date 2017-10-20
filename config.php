64f2a353174540c19d7d2f7ceac214adc6a70835
Diego Tsuyoshi <dttsuyoshi@gmail.com>
2017-10-20 17:07:09 -0300

    Inicializando variáveis de acesso ao banco

--- config.php
+++ config.php
@@ -2,3 +2,9 @@
 
 define('URL', 'http://www.blog-local.com/');
 define('LIBS', 'libs/');
+
+define('DB_NAME', 'blog');
+define('DB_TYPE', 'mysql');
+define('DB_HOST', '127.0.0.1');
+define('DB_USER', 'root');
+define('DB_PASS', '');
