26bc720f1d33c38103e77572c275996d24582ca9
Diego Tsuyoshi <dttsuyoshi@gmail.com>
2017-10-24 16:47:43 -0300

    Corrigingo return da função find no post_model

--- models/post_model.php
+++ models/post_model.php
@@ -11,7 +11,10 @@ class Post_Model extends Model {
     }
 
     public function find($id) {
-        return $this->database->select('SELECT * from posts where posts.id = '.$id)[0];
+        $post = $this->database->select('SELECT * from posts where posts.id = '.$id);
+
+        // Returning $post[0] because the command above returns a array
+        return $post[0];
     }
 
     public function create($data) {
