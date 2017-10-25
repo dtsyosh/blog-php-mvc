37e14f0a59da0cee70d03f7979e7c073f33d06ee
gabybprado <gabryelebenites@gmail.com>
2017-10-25 16:06:43 -0300

    criado funcao create

---
+++ controllers/user.php
@@ -0,0 +1,39 @@
+<?php
+
+class User extends Controller {
+
+    public function __construct(){
+        parent::__construct();
+    }
+
+    public function index(){
+        $this->view->title = 'User';
+        $this->view->render('user/index');
+    }
+
+    public function create(){
+        if($SERVER['REQUEST_METHOD'] === 'GET'){
+            $this->view->title = 'Registrar';
+            $this->view->render('user/create');
+        }else{
+            
+            $data = array();
+            $data['name'] = $_POST['name'];
+            $data['email'] = $_POST['email'];
+            $data['password'] = $_POST['password'];
+    
+            $this->model->create($data); // check if this model exists
+            header('location: ' . URL . 'user');
+        }
+    }
+
+    public function update($id) {
+        # update
+    }
+
+    public function destroy($id) {
+        # delete
+    }
+
+
+}
