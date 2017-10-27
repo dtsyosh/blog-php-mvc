d8f50178938a2a0d1cf5ea45eac4c09ed8177298
Diego Tsuyoshi <dttsuyoshi@gmail.com>
2017-10-27 19:21:54 -0300

    Adicionando hasRole() em user

--- database.sql
+++ database.sql
@@ -82,12 +82,10 @@ insert into `permission_role` (`permission_id`, `role_id`) values
 (1, 1),(1, 2),(1, 3),(2, 1),(2, 2),(3, 1),(3, 2),(4, 1),(4, 2),(4, 3),(4, 4),(5, 1),(5, 2),(5, 3),(5, 4),(6, 1),(6, 2),(6, 3),(6, 4),(7, 1),(8, 1),(9, 1);
 
 # Some data _-_-_-_-_
-insert into `users` (`name`, `email`, `password`) values
- ('Diego Tsuyoshi', 'dttsuyoshi@gmail.com', '123456'),
- ('Geovana Helena', 'geovanahsps@gmail.com', '123456');
+insert into `users` (`name`, `email`, `password`, `role_id`) values
+ ('Diego Tsuyoshi', 'dttsuyoshi@gmail.com', '123456', 1),
+ ('Geovana Helena', 'geovanahsps@gmail.com', '123456', 2);
 
-insert into `role_user` (`role_id`, `user_id`) values
-(3, 1), (4, 2);
 
  insert into `posts` (`title`, `body`, `user_id`) values
  ('Lorem Ipsum', 'In sem justo, commodo ut, suscipit at, pharetra vitae, orci. Duis sapien nunc, commodo et, interdum suscipit, sollicitudin et, dolor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam id dolor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos. Mauris dictum', 1),
