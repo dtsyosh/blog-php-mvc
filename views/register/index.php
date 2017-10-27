53d805391de20faf0ac6213dba801e80fbe18e1c
Gabryele Benites <gabryelebenites@gmail.com>
2017-10-27 19:07:39 -0300

    deixando o blog mais bonito

--- views/register/index.php
+++ views/register/index.php
@@ -1,14 +1,17 @@
-<h1> Cadastrar Usuário </h1>
-
-<form action="<?php echo URL;?>register/create" method="post">
-    <div class="form-group">
-        Nome<br> <input type="text" name="name" id="">    
+<div class="card my-4">
+    <h5 class="card-header">Cadastrar</h5>
+    <div class="card-body">
+        <form action="<?php echo URL;?>register/create" method="post">
+            <div class="form-group">
+            Nome<br> <input type="text" name="name" id="">    
+            </div>
+            <div class="form-group">
+            Email<br> <input type="email" name="email" id="">
+            </div>
+             <div class="form-group">
+            Senha<br> <input type="password" name="password" id="">
+            </div>
+            <input type="submit" value="Enviar">       
+        </form>
     </div>
-    <div class="form-group">
-        Email<br> <input type="email" name="email" id="">
-    </div>
-    <div class="form-group">
-        Senha<br> <input type="password" name="password" id="">
-    </div>
-    <input type="submit" value="Enviar">
-</form>
+</div>
